db559edb6fb6abe5ad86abc507f421656c4a7b79
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-02-12 22:43:10 +0200

    added the useReducer hook

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,37 @@
 import CartContext from "./cart-context";
+import React, { useReducer } from "react";
+
+const defaultCartState={
+  items:[],
+  totalAmount:0
+}
+const cartReducer=(state, action)=>{
+  if(action.type==="ADD"){
+    const updatedItems=state.items.concat(action.item)
+    const updatedTotalAmount=state.totalAmount+action.item.price*action.item.amount
+    return{
+      items:updatedItems,
+      totalAmount:updatedTotalAmount
+    }
+  }
+return defaultCartState
+}
+
 const CartProvider = (props) => {
+   const[cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
     const addItemToCartHandler=item=>{
+      dispatchCartAction({type:"ADD",item:item})
 
     };
     const removeItemFromCartHandler=id=>{
-
+      dispatchCartAction({type:"DELETE",id:id})
     };
     const cartContext={
-        items:[],
-        totalAmout:0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    }
+      items:cartState.items,
+      totalAmount:cartState.totalAmount,
+      addItem: addItemToCartHandler,
+      removeItem: removeItemFromCartHandler
+  }
   return <>
   <CartContext.Provider value={cartContext}>
     {props.children}
